a832352cb337b10a04401ab61c2619003aa352fc
Rob Cho <robhcho@gmail.com>
2017-11-28 22:35:05 -0500

    Display name, email, and phone_number for first contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,10 @@
 class ContactsController < ApplicationController
   def contact_info
     contact_1 = Contact.first
-    render json: {
-      # message: "hello"
-      # contact_info: Contact.first 
-      name: contact_1.name
-      # email: contact_1.
-      }
+    render json: { 
+      name: contact_1.name,
+      email: contact_1.email,
+      phone_number: contact_1.phone_number
+    }
   end
 end
